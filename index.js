4df2fe938f0fab4816683eb04e1e25f567e484e7
Roman Surinov <romasurinov13@mail.ru>
2022-08-21 23:29:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+console.log('extension');
+
+const bg = document.querySelector('body');
+const windowExt = document.createElement('div')
+windowExt.classList.add('red_bg')
+bg.appendChild(windowExt)
+
+
